8428f2c59dc4a1f48275d5b57f7c9875cbbf8bb8
Tomas Livora <tomas.livora@rbflabs.com>
2022-06-16 09:04:21 +0200

    chore: add more items to .gitignore

--- demo/app-ts/src/moolaMinterConstants.mjs
+++
@@ -1,9 +0,0 @@
-// GENERATED FROM /Users/robertbrada/Documents/Coding/RBF/monorepo/contracts/contract-1/deploy.js
-export default {
-  "CONTRACT_NAME": "moolaMinter",
-  "INSTALLATION_BOARD_ID": "board03125",
-  "INSTANCE_BOARD_ID": "board00126",
-  "TOKEN_ISSUER_BOARD_ID": "board03523",
-  "TOKEN_BRAND_BOARD_ID": "board05024",
-  "INVITE_BRAND_BOARD_ID": "board02810"
-};
