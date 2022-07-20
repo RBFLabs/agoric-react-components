9e45886815c4e4ef1f4b65fbe36858421f9d18e5
Pernick <k.kosina@outlook.com>
2022-07-20 10:39:24 +0200

    removed unnecessary short-unique-id dependency

--- packages/rbflabs-agoric-react-components/src/contexts/wallet/hooks/use-wallet-notifications/use-wallet-notifications.ts
+++ packages/rbflabs-agoric-react-components/src/contexts/wallet/hooks/use-wallet-notifications/use-wallet-notifications.ts
@@ -1,12 +1,10 @@
 import { useEffect, useRef } from 'react';
-import ShortUniqueId from 'short-unique-id';
+import { randomUUID as uid } from 'crypto';
 import { usePrevious } from '../../../../hooks';
 import { keys } from '../../../../utils/object';
 import { WalletNotification, WalletNotificationGroup } from '../../types';
 import { useWallet } from '../use-wallet';
 import { getConnectNotification, getOfferNotification, getUpdatedOffer } from './utils';
-
-const uid = new ShortUniqueId({ length: 10 });
 
 function isSameNotification(a: WalletNotification, b?: WalletNotification) {
   return b && keys(a).every(key => a[key] === b[key]);
